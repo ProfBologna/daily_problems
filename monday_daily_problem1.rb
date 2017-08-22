5bf2c406778b8ca79c16988c2c7d0a2baec34d83
ProfBologna <sam.aporia@gmail.com>
2017-08-22 11:29:03 -0500

    Moved some files to new repo

--- monday_daily_problem1.rb
+++
@@ -1,3 +0,0 @@
-numbers = [1, 2, 4, 2]
-numbers.map! {|x| x * 2}
-p numbers
--- tuesday_daily_week_1.rb
+++
@@ -1,6 +0,0 @@
-fruits = [
-  {"name" => "apple", "color" => "red"},
-  {"name" => "banana", "color" => "yellow"},
-  {"name" => "grape", "color" => "purple"}
-]
-
