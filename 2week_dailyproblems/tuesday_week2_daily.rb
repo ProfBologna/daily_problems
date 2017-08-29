8b88a6346694546b08f50aa22b8cb69cd73acffb
ProfBologna <sam.aporia@gmail.com>
2017-08-29 17:36:21 -0500

    Tuesday daily completed.

---
+++ 2week_dailyproblems/tuesday_week2_daily.rb
@@ -0,0 +1,9 @@
+movies = [
+  {id: 1, title: "Die Hard", rating: 4.0},
+  {id: 2, title: "Bad Boys", rating: 5.0},
+  {id: 3, title: "The Chamber", rating: 3.0},
+  {id: 4, title: "Fracture", rating: 2.0}
+]
+
+x = movies.select { |movie| movie[:title].downcase.include? "b"}
+p x.map { |y| y[:id] }
